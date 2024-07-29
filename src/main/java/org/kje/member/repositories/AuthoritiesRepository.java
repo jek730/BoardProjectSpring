05edb068b7cc551bf45fc302e5d3d955a2d5b581
jek730 <jejek730@gmail.com>
2024-07-29 16:43:08 +0900

    스프링 부트 - Spring Security(회원가입 검증)

--- src/main/java/org/kje/member/repositories/AuthoritiesRepository.java
+++ src/main/java/org/kje/member/repositories/AuthoritiesRepository.java
@@ -2,8 +2,13 @@ package org.kje.member.repositories;
 
 import org.kje.member.entities.Authorities;
 import org.kje.member.entities.AuthoritiesId;
+import org.kje.member.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 public interface AuthoritiesRepository extends JpaRepository<Authorities, AuthoritiesId>, QuerydslPredicateExecutor<Authorities> {
+
+    List<Authorities> findByMember(Member member);
 }
