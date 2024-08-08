eb5d1cdb486fa0e4317a11ea4cc47f92a03c25a5
jek730 <jejek730@gmail.com>
2024-08-08 19:19:38 +0900

    지도 API - 마커에 클릭 이벤트 등록하기

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -9,7 +9,8 @@ const mapLib = {
     *               - center: { lat: 위도, lng: 경도} - 필수
     *               - zoom : 확대 정도(1~10) / 숫자가 작을 수록 확대
                     - markerImage: 공통 마커 이미지 주소, 개별 마커 이미지가 있는 경우는 그걸로 대체,
-    *               - marker : [{ lat: 위도, lng: 경도, info: html 데이터(인포윈도우), image: 이미지 주소 - 마커이미지}]
+    *               - marker : [{ lat: 위도, lng: 경도,
+                                info: { content: html 데이터(인포윈도우), clickable: true|false - true(마커 클릭시 노출) }, image: 이미지 주소 - 마커이미지}]
     */
     load(mapId, width = 300, height = 300, options) {
         const mapEl = document.getElementById(mapId);
@@ -33,18 +34,45 @@ const mapLib = {
         // 마커 출력 처리 S
         if (marker) {
             if (!Array.isArray(marker)) marker = [marker];
-
             const markers = marker.map(m => {
-                const opt = { position: new kakao.maps.LatLng(m.lat, m.lng)};
+                const { lat, lng, image, info } = m;
+                const opt = { position: new kakao.maps.LatLng(lat, lng)};
+
 
                 // 마커 이미지 처리
-                const mi = markerImage ? markerImage : m.image;
+                const mi = markerImage ? markerImage : image;
                 if (mi) {
                     const mImage = new kakao.maps.MarkerImage(mi, new kakao.maps.Size(64, 69), {offset: new kakao.maps.Point(27, 69)});
                     opt.image = mImage;
                 }
 
                 const _marker = new kakao.maps.Marker(opt);
+
+                // 인포 윈도우 처리
+                if (info?.content) {
+                    const { content, clickable, removable } = info;
+
+                    const infoWindow = new kakao.maps.InfoWindow({
+                        content,
+                        removable: Boolean(removable),
+                    });
+
+                    if (clickable) { // 마커 클릭시 노출
+                        kakao.maps.event.addListener(_marker, "click", function() {
+                            if (_marker.isInfoWindowOpen) {
+                                infoWindow.close();
+                                _marker.isInfoWindowOpen = false;
+                            } else {
+                                infoWindow.open(map, _marker);
+                                _marker.isInfoWindowOpen = true;
+                            }
+                        });
+                    } else { // 바로 노출
+                        infoWindow.open(map, _marker);
+                    }
+                }
+
+
 
                 _marker.setMap(map);
 
@@ -53,5 +81,18 @@ const mapLib = {
 
         } // endif
         // 마커 출력 처리 E
+    },
+    /**
+    * 현재 위치 기반으로 중심위치를 잡고 지도 출력
+    *
+    */
+    loadCurrentLocation(mapId, width = 300, height = 300, options) {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const { latitude, longitude } = pos.coords;
+            options = options ?? {};
+            options.center = { lat: latitude, lng: longitude };
+
+            mapLib.load(mapId, width, height, options);
+        });
     }
 };
