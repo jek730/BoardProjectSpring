078172f067a79b98b8a7857c3b1c5dc79d5d8bd7
jek730 <jejek730@gmail.com>
2024-08-09 14:04:34 +0900

    페이징 - Pagination

---
+++ src/main/java/org/kje/global/ListData.java
@@ -0,0 +1,11 @@
+package org.kje.global;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ListData<T> {
+    private List<T> items; // 목록 데이터
+    private Pagination pagination;
+}
